95af576e14cac9f5da55b9ab44774f44dcdbbd10
ani_ghost <57623270+anighost1@users.noreply.github.com>
2023-08-16 08:23:25 +0530

    started

--- src/Pages/modules/views/AppAppBar.js
+++ src/Pages/modules/views/AppAppBar.js
@@ -5,50 +5,52 @@ import AppBar from '../components/AppBar';
 import Toolbar from '../components/Toolbar';
 
 const rightLink = {
-  fontSize: 16,
-  color: 'common.white',
-  ml: 3,
+    fontSize: 16,
+    color: 'common.white',
+    ml: 3,
 };
 
 function AppAppBar() {
-  return (
-    <div>
-      <AppBar position="fixed">
-        <Toolbar sx={{ justifyContent: 'space-between' }}>
-          <Box sx={{ flex: 1 }} />
-          <Link
-            variant="h6"
-            underline="none"
-            color="inherit"
-            href="/premium-themes/onepirate/"
-            sx={{ fontSize: 24 }}
-          >
-            {'onepirate'}
-          </Link>
-          <Box sx={{ flex: 1, display: 'flex', justifyContent: 'flex-end' }}>
-            <Link
-              color="inherit"
-              variant="h6"
-              underline="none"
-              href="/premium-themes/onepirate/sign-in/"
-              sx={rightLink}
-            >
-              {'Sign In'}
-            </Link>
-            <Link
-              variant="h6"
-              underline="none"
-              href="/premium-themes/onepirate/sign-up/"
-              sx={{ ...rightLink, color: 'secondary.main' }}
-            >
-              {'Sign Up'}
-            </Link>
-          </Box>
-        </Toolbar>
-      </AppBar>
-      <Toolbar />
-    </div>
-  );
+    return (
+        <div>
+            <AppBar position="fixed">
+                <Toolbar sx={{ justifyContent: 'center' }}>
+                    {/* <Box sx={{ flex: 1 }} /> */}
+                    <Link
+                        variant="h6"
+                        underline="none"
+                        color="inherit"
+                        href="/premium-themes/onepirate/"
+                        sx={{
+                            fontSize: 24
+                        }}
+                    >
+                        <Box component='span' sx={{ color: 'secondary.main' }}>A</Box>nil <Box component='span' sx={{ color: 'secondary.main' }}>T</Box>gga
+                    </Link>
+                    {/* <Box sx={{ flex: 1, display: 'flex', justifyContent: 'flex-end' }}>
+                        <Link
+                            color="inherit"
+                            variant="h6"
+                            underline="none"
+                            href="/premium-themes/onepirate/sign-in/"
+                            sx={rightLink}
+                        >
+                            {'Sign In'}
+                        </Link>
+                        <Link
+                            variant="h6"
+                            underline="none"
+                            href="/premium-themes/onepirate/sign-up/"
+                            sx={{ ...rightLink, color: 'secondary.main' }}
+                        >
+                            {'Sign Up'}
+                        </Link>
+                    </Box> */}
+                </Toolbar>
+            </AppBar>
+            <Toolbar />
+        </div>
+    );
 }
 
 export default AppAppBar;
